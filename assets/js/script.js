f6d33aad2b2f23db8479e4b83347f482c2c6a250
Shad <farshadkalathingal@gmail.com>
2020-11-06 21:30:41 +0530

    Features

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,9 @@ AOS.init({
 jQuery(function($) {
 	
     'use strict';
+    Active_Scroll();
     Window_Scroll();
-    // // Smooth_Scroll();
-    // bxSlider();
-    Stats_Counter();
+    Smooth_Scroll();
     Owl_Carousel();
 });
 
@@ -36,27 +35,8 @@ var Smooth_Scroll = function() {
 	  });
 };
 
-// bxSlider
-
-var bxSlider = function() {
-    $('.bxslider').bxSlider({
-        minSlides: 1,
-		maxSlides: 5,
-        slideWidth: 257,
-		slideMargin: 17,
-		ticker: true,
-		tickerHover: true,
-		speed: 20000,
-		useCSS: false, 
-        infiniteLoop: false,
-        auto: true,
-        autoControls: true
-    });
-
-};
-
 var Owl_Carousel = function() {
-    $('#features .owl-carousel').owlCarousel({
+    $('#courses .owl-carousel').owlCarousel({
         loop:true,
         margin:10,
         navText:["<i class='fas fa-chevron-left'></i>" , "<i class='fas fa-chevron-right'></i>"],
@@ -98,37 +78,7 @@ var Owl_Carousel = function() {
 
 };
 
-// Stats Counter
 
-var Stats_Counter = function() {
-	$('#stats').waypoint( function( direction ) {
-		
-		if( direction === 'down') {
-            setTimeout(function() {
-                $('.counting').each(function() {
-                    var $this = $(this),
-                        countTo = $this.attr('data-count');
-                    
-                    $({ countNum: $this.text()}).animate({
-                        countNum: countTo
-                    },
-                
-                    {
-                
-                        duration: 3000,
-                        easing:'linear',
-                        step: function() {
-                            $this.text(Math.floor(this.countNum) + '+');
-                        },
-                        complete: function() {
-                            $this.text(this.countNum + '+');
-                            //alert('finished');
-                        }
-                
-                    });  
-                });
-            }, 500);
-			}
-		
-		}, { offset: '95%' } );
-};
+var Active_Scroll = function() {
+    
+}
