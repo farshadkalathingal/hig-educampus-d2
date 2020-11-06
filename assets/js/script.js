e02b55e983a1fcaa57ec8a3a02a2529f39af21f3
Shad <farshadkalathingal@gmail.com>
2020-11-06 16:25:38 +0530

    Student says

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,6 +82,20 @@ var Owl_Carousel = function() {
             }
         }
     });
+
+    $('#students .owl-carousel').owlCarousel({
+        items: 1,
+        loop:true,
+        margin:10,
+        dot: true,
+        // nav: true,
+        // navText:["<i class='fas fa-chevron-left'></i>" , "<i class='fas fa-chevron-right'></i>"],
+        autoplay:true,
+        autoplayTimeout:2500,
+        autoplayHoverPause:true,
+        autoplaySpeed: 1000,
+    });
+
 };
 
 // Stats Counter
